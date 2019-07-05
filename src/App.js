fea1cf2f72dda77bd4eeaeef689d61d5c8bc774b
Agustin <agus.jimenez.caba@gmail.com>
2019-07-04 21:59:40 -0400

    adding base template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import 'typeface-roboto'
-import Typography from '@material-ui/core/Typography'
 import { hot } from 'react-hot-loader'
+import ErpPage from './pages/Erp'
 
-const App = () => (
-  <Typography variant="h4" gutterBottom>
-    Hello, World
-  </Typography>
-);
 
-export default hot(module)(App);
+class App extends React.Component {
+  render() {
+    return <ErpPage />
+  }
+}
+
+export default hot(module)(App)
